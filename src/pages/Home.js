e9a27e0fd7ab4832d1a3e8b7fb800d90940d7902
Daudmir <74597831+Daudmir@users.noreply.github.com>
2023-03-02 22:04:55 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import Nav from '../components/Nav'
+import Nav from '../Components/Nav'
+import { useState, useEffect } from 'react';
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+     fetch('https://api4286.s3.ap-south-1.amazonaws.com/products.json')
+        .then((response) => response.json())
+        .then((data) => {
+           console.log(data);
+           setPosts(data);
+        })
+        .catch((err) => {
+           console.log(err.message);
+        });
+  }, []);
   return (
     <>
-    <Nav />
-    Home
+    <Nav/>
+    <div className="posts-container">
+      {posts.map((post) => {
+         return (
+            <div className="post-card" key={post.title}>
+               <h2 className="post-title">{post.title}</h2>
+               <p className="post-body">{post.body}</p>
+            </div>
+         );
+      })}
+   </div>
     </>
   )
 }
